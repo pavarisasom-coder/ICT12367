e208f9ea5baa8ac3dde351754f7b9bde8cf426ff
pavarisasom-coder <pavarisa.som@spumail.net>
2026-02-04 11:51:35 +0700

    Add files via upload

---
+++ lab4/L4app.js
@@ -0,0 +1,16 @@
+const smallimgs = document.querySelectorAll(".gallery img")
+const fullimg = document.querySelector(".full-image")
+const modal = document.querySelector(".modal")
+smallimgs.forEach(img => {
+    img.addEventListener("click", () => {
+        const fullsize = img.getAttribute("alt")
+        const path = `foods-images/full/${fullsize}.jpg`
+        fullimg.src = path
+        modal.classList.add("open")
+    })
+})
+modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")){
+        modal.classList.remove("open")
+    }
+})
